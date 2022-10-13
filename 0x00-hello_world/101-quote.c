4eda7be262fa8cc484e723150f1a84ceae60b024
Ifemier <ifeoluwafashina03@gmail.com>
2022-10-13 16:20:47 -0700

    Ans 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
- * main- prints out the last part
- *
+ * main- Entry point
+ * @void: parameter
  * Return: 1 (success)
-*/
+ */
 
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		printf(stderr, "and that piece of art is useful\" - \
+				Dora Korpar, 2015-10-19\n");
 		return (1);
 }
