1937ee756b819c6140debf12a44fe533705ef3b0
Ifemier <ifeoluwafashina03@gmail.com>
2022-10-13 15:17:14 -0700

    Ans 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *  * main - prints "Programming is like building a multilingual puzzle
  *   * @void: Function argument
  *    * Return: 0 if the code runs successfuly. 1 otherwise
- *     */
+*/
 
 int main(void)
 {
