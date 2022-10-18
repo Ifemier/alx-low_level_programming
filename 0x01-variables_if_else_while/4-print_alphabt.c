f89c5e5b63d93543851efb00fb9b1c5a9e72eb36
Ifemier <ifeoluwafashina03@gmail.com>
2022-10-17 21:29:44 -0700

    Task 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
- *  main - entry point
- *  Return: always return 0
- */
+ * main - entry point
+ * Return: alway returns 0
+*/
 int main(void)
 {
 	char a;
 
 	for (a = 'a'; a <= 'z' ; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
 		putchar(a);
+	}
 	putchar('\n');
 	return (0);
 }
