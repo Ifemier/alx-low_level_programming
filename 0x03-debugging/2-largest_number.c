1bce450cfa27099da69e497e24f6f00bfff7cbf1
Ifemier <ifeoluwafashina03@gmail.com>
2022-10-20 12:01:26 -0700

    Task 3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,22 +10,23 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+	int largest;
 
-if (a >= b && a >= c)
-{
-largest = a;
-}
-else if (b >= a && b >= c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-
-return (largest);
-}
-
-void print_remaining_days(int month,
+	if (a > b)
+	{
+		if (b > c)
+			largest = a;
+		else if (a > c)
+			largest = a;
+		else 
+			largest = c;
+	}
+	else
+	{	
+		if (b < c)
+			largest = c;
+		else  
+			largest = b;
+	}
+	return (largest);
+}	
